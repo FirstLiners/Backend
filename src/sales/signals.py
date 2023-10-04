f289dfae0f45d1683910756c3d5b40b5f95762c8
PentiukPavel <pentukpavel@yandex.ru>
2023-10-04 17:49:49 +0300

    statistics

--- src/sales/signals.py
+++ src/sales/signals.py
@@ -18,7 +18,7 @@ def forecast_real_sales_filling(sender, instance, created, **kwargs):
         forecast = Forecast.objects.filter(
             sku=instance.sku,
             store=instance.store,
-            date=instance.date - timedelta(days=1)
+            date=instance.date - timedelta(days=1),
         ).first()
         if forecast.exists():
             forecast.next_day_real_sale = instance.sales_in_units
